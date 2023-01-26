48b1c4b36f120ed54221ebfbb0d19b052f0c8186
OGWhiskers <bayandadlamini02@gmail.com>
2023-01-26 12:03:15 +0200

    Add keep score function
    Function logs the result of single round function(score of winner and
    loser) Also called function into single round function.

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let computerScore = 0;
+let playerScore = 0;
+
+
 function getComputerChoice(){
 
     let rps = ['rock','paper','scissors'];
@@ -15,32 +19,67 @@ function SingleRound() {
     let computerSelection = getComputerChoice();
     
     if(playerSelection === 'rock' && computerSelection === 'paper'){
-
+        
+        computerScore += 1
+        keepScore();
         return 'You Lose! Paper beats Rock'
+       
 
     }else if(playerSelection === 'paper' && computerSelection === 'scissors'){
 
+        computerScore += 1
+        keepScore();
         return 'You Lose! Scissors beats Paper'
 
     }else if(playerSelection === 'scissors' && computerSelection === 'rock'){
 
+        computerScore += 1
+        keepScore();
         return 'You Lose! Rock beats Scissors'
+        
 
     }else if(playerSelection === 'rock' && computerSelection === 'scissors'){
 
+        playerScore += 1
+        keepScore();
         return 'You Win! Rock beats Scissors'
+        
 
     }else if(playerSelection === 'paper' && computerSelection === 'rock'){
 
+        playerScore += 1
+        keepScore();
         return 'You Win! Paper beats Rock'
+        
 
     }else if(playerSelection === 'scissors' && computerSelection === 'paper'){
 
+        playerScore += 1
+        keepScore();
         return 'You Win! Scissors beats Paper'
+       
 
     } else {
-
+        keepScore();
         return 'Its a draw. Try again'
 
     }
 }
+
+function keepScore () {
+    if (playerScore <= 5 || computerScore <= 5) {
+
+        console.log(`Current Score: Your Score is ${playerScore} 
+        and the CPU's score is ${computerScore}`);
+
+    }
+}
+
+function game () {
+    for (let i = 0; i < 5; i++) {
+        console.log(SingleRound());;
+    }
+}
+game();
+
+
