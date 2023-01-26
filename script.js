a0f739e675e402c2b83ac8d8ebadd5d46b5992d8
OGWhiskers <bayandadlamini02@gmail.com>
2023-01-26 11:15:22 +0200

    Add function to play a single round

--- script.js
+++ script.js
@@ -6,6 +6,41 @@ function getComputerChoice(){
 
     return rps[choice];
 
+};
+
+function SingleRound() {
+
+    let playerchoice = 'ROck';
+    let playerSelection = playerchoice.toLowerCase();
+    let computerSelection = getComputerChoice();
+    
+    if(playerSelection === 'rock' && computerSelection === 'paper'){
+
+        return 'You Lose! Paper beats Rock'
+
+    }else if(playerSelection === 'paper' && computerSelection === 'scissors'){
+
+        return 'You Lose! Scissors beats Paper'
+
+    }else if(playerSelection === 'scissors' && computerSelection === 'rock'){
+
+        return 'You Lose! Rock beats Scissors'
+
+    }else if(playerSelection === 'rock' && computerSelection === 'scissors'){
+
+        return 'You Win! Rock beats Scissors'
+
+    }else if(playerSelection === 'paper' && computerSelection === 'rock'){
+
+        return 'You Win! Paper beats Rock'
+
+    }else if(playerSelection === 'scissors' && computerSelection === 'paper'){
+
+        return 'You Win! Scissors beats Paper'
+
+    } else {
+
+        return 'Its a draw. Try again'
+
+    }
 }
-
-
